4cc0e2027cadd634aaabad92a11ed9b77872e60b
Jaiderin123 <torrezjaider10@gmail.com>
2026-05-20 09:40:59 -0500

    First Commit

---
+++ src/main/java/com/softwarecolombia/projectmanager/SoftwareColombiaTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.softwarecolombia.projectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareColombiaTechnicalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareColombiaTechnicalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softwarecolombia/projectmanager/SoftwareColombiaTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarecolombia.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareColombiaTechnicalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
